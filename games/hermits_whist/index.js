e247c9c9d650084397019eeda343bf3fc3f31217
Webdesk-me <atelford@webdesk.me>
2021-05-13 06:26:05 -0400

    background

--- games/hermits_whist/index.js
+++ games/hermits_whist/index.js
@@ -10,7 +10,8 @@ var config = {
         },
         scene: {
             preload: preload,
-            create: create
+            create: create,
+            update: update
         }
     };
 
@@ -18,18 +19,18 @@ var config = {
 
     function preload ()
     {
-        this.load.setBaseURL('http://labs.phaser.io');
+        //this.load.setBaseURL('http://labs.phaser.io');
 
-        this.load.image('sky', 'assets/skies/space3.png');
-        this.load.image('logo', 'assets/sprites/phaser3-logo.png');
-        this.load.image('red', 'assets/particles/red.png');
+        this.load.image('sky', 'assets/backgrounds/back1.jpg');
+        //this.load.image('logo', 'assets/sprites/phaser3-logo.png');
+        //this.load.image('red', 'assets/particles/red.png');
     }
 
     function create ()
     {
         this.add.image(400, 300, 'sky');
 
-        var particles = this.add.particles('red');
+        /*var particles = this.add.particles('red');
 
         var emitter = particles.createEmitter({
             speed: 100,
@@ -43,5 +44,8 @@ var config = {
         logo.setBounce(1, 1);
         logo.setCollideWorldBounds(true);
 
-        emitter.startFollow(logo);
+        emitter.startFollow(logo);*/
+    }
+    function update ()
+    {
     }
